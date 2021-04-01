358ccbc22a74717512777f3451639c1fc40c0256
Dmitriy <flathome86@mail.ru>
2021-04-01 21:09:58 +0300

    homework1

---
+++ homework1.py
@@ -0,0 +1,10 @@
+a = 10
+b = 'Яблок'
+print(a, b)
+
+a = int(input('Введите число от 0 до 9: '))
+b = int(input('Введите число от 0 до 30: '))
+name = input('Введите ваше имя: ')
+surname = input('Введите вашу фамилию: ')
+print(a + b)
+print(name, surname)
