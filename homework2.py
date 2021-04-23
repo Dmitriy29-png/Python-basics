2b8fc5b5d3b99838f25098e3e2e57aa9b9b80dd5
Dmitriy <flathome86@mail.ru>
2021-04-23 18:23:20 +0300

    homework2

---
+++ homework2.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from abc import abstractmethod
+
+
+class Clothes:
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @abstractmethod
+    def cloth_out(self):
+        pass
+
+
+class Coat(Clothes):
+
+    def __init__(self, size):
+        super(Coat, self).__init__(size)
+
+    def cloth_out(self):
+        return f'Площадь на пальто: {self.size / 6.5 + 0.5}'
+
+
+class Suit(Clothes):
+
+    def __init__(self, size):
+        super(Suit, self).__init__(size)
+
+    def cloth_out(self):
+        return f'Площадь на костюм: {self.size * 2 + 0.3}'
+
+    @staticmethod
+    def __define_size_by_value(val):
+        return (val - 0.3) // 2
+
+    @property
+    def size_out(self):
+        return self.size
+
+    @size_out.setter
+    def size_out(self, val):
+        tmp_size = int(self.__define_size_by_value(val))
+        print(f'Этого хватает на {tmp_size}-й размер.')
+        if tmp_size < 1:
+            print('невозможно использовать это количество ткани.')
+        else:
+            print('Применяем.')
+            self.size = tmp_size
+
+
+
+
+
+c = Coat(12)
+print(c.cloth_out())
+
+s = Suit(30)
+print(s.cloth_out())
+print(s.size_out)
+print(s.cloth_out())
+s.size_out = 36
